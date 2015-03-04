9e6380095d4830ddaf8bfc03e8d3e8cdf67ec33f
David Kirwan <dkirwan@tssg.org>
2015-03-04 23:56:15 +0000

    - Added chararacter limit on the number of characters being converted

--- sinatra_morse.rb
+++ sinatra_morse.rb
@@ -45,7 +45,9 @@ class App < Sinatra::Base
   end
 
   post '/generate' do
-    [400, {"Content-Type" => "text/plain"},["400 Bad Request"]] unless not params["morse_input"].nil?
+    throw :halt, [400, {"Content-Type" => "text/plain"},["400 Bad Request"]] unless not params["morse_input"].nil? 
+    # To set a limit on the number of characters allowed to be converted, uncomment
+    #throw :halt, [400, {"Content-Type" => "text/plain"},["400 limit the number of characters to be 100 or less"]] unless params["morse_input"].length <= 100
     settings.log.debug "morse_input: " + params["morse_input"]
     data = Morse.to_morse(params["morse_input"])
     settings.log.debug "to_morse: " + data
